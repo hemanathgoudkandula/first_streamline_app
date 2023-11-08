a2ff597c8d2446cc5618556d5ebef65574495cdf
hemanathgoudkandula <95487481+hemanathgoudkandula@users.noreply.github.com>
2023-11-08 18:49:22 +0530

    Update streamlit_app.py
    corrected the read csv and display the fruit menu

--- streamlit_app.py
+++ streamlit_app.py
@@ -9,10 +9,8 @@ st.text('🥗 Kale, Spinach & Rocket Smoothie')
 st.text('🐔Hard-Boiled Free-Range Egg')
 st.text('🥑🍞Avocado Toast')
 
+st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
 
 my_fruit_list = pd.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
 
 st.dataframe(my_fruit_list)
-
-st.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
-
