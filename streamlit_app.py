af5cc86195bbab0028b5f856f406ff3fc6651283
hemanathgoudkandula <95487481+hemanathgoudkandula@users.noreply.github.com>
2023-11-09 20:35:22 +0530

    Update streamlit_app.py
    take fruit choice as input and display the output in table

--- streamlit_app.py
+++ streamlit_app.py
@@ -25,7 +25,10 @@ st.dataframe(fruits_to_show)
 
 st.header("Fruityvice Fruit Advice!")
 
-fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+"watermelon")
+fruit_choice = st.text_input('What fruit would you like information about?','Kiwi')
+st.write('The user entered ', fruit_choice)
+
+fruityvice_response = requests.get("https://fruityvice.com/api/fruit/"+fruit_choice)
 #st.text(fruityvice_response.json())
 
 #Normalize the json value
