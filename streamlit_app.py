151b3cb4f6f70752b04d94bbfe347456ee49ae39
hemanathgoudkandula <95487481+hemanathgoudkandula@users.noreply.github.com>
2023-11-11 22:56:43 +0530

    Update streamlit_app.py
    updated the insert script to take the new_fruit and updated the button text

--- streamlit_app.py
+++ streamlit_app.py
@@ -54,20 +54,22 @@ def get_fruit_load_list():
       my_cur.execute("select * from fruit_load_list")
       return my_cur.fetchall()
 
-if st.button("Get Fruit Load List"):
+if st.button("Get Fruit List"):
    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
    my_data_rows= get_fruit_load_list()
+   my_cnx.close()
    st.dataframe(my_data_rows)
 
 
 def insert_row_snowflake(new_fruit):
    with my_cnx.cursor() as my_cur:
-      my_cur.execute("insert into FRUIT_LOAD_LIST values ('from st')")
+      my_cur.execute("insert into FRUIT_LOAD_LIST values ('" + new_fruit + "')")
       return "Thanks for adding "+ new_fruit
 
 add_my_fruit = st.text_input('What fruit would you like to add?')
 if st.button("Add a Fruit to the List"):
    my_cnx = snowflake.connector.connect(**st.secrets["snowflake"])
    back_from_function = insert_row_snowflake(add_my_fruit)   
+   my_cnx.close()
    st.text(back_from_function)
 st.stop()
